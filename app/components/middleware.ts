de7c52f3ee8a55c1d2250f414855f5cc93fc2501
sunilworkrow <ksunil@workrow.io>
2026-05-12 17:40:14 +0530

    6a

---
+++ app/components/middleware.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+
+    const basicAuth = request.headers.get('authorization')
+
+    if (basicAuth) {
+
+        const authValue = basicAuth.split(' ')[1]
+
+        const [user, pwd] = atob(authValue).split(':')
+
+        if (
+            user === 'admin' &&
+            pwd === 'admin'
+        ) {
+
+            return NextResponse.next()
+
+        }
+
+    }
+
+    return new NextResponse(
+        'Authentication required',
+        {
+            status: 401,
+            headers: {
+                'WWW-Authenticate': 'Basic realm="Secure Area"',
+            },
+        }
+    )
+}
+
+export const config = {
+    matcher: '/:path*',
+}
